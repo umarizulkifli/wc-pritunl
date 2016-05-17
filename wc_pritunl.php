41aa6aad11d35db3362c8606441bda5dd9100732
xUmaRix <umarizulkifli@gmail.com>
2016-05-17 15:15:20 +0800

    First Commit

---
+++ wc_pritunl.php
@@ -0,0 +1,27 @@
+<?php
+/*
+Plugin Name: WC Pritunl
+Plugin URI: http://freedom.globaltransit.net/
+Description: Integration with Pritunl API
+Author: Mohd Shahmi
+Author URI: http://freedom.globaltransit.net
+Version: 1.0
+Text Domain: wootickets-asia-info
+
+Copyright: � 2016 xUmaRix
+License: GNU General Public License v3.0
+License URI: http://www.gnu.org/licenses/gpl-3.0.html
+*/
+//referal from : http://stackoverflow.com/questions/28218580/woocommerce-hook-for-after-payment-complete-actions
+
+
+add_filter( 'woocommerce_order_status_completed', 'create_user', 1);
+function create_user( $order_id ){
+  global $woocommerce;
+  
+  $order = new WC_Order( $order_id );
+  $fname = $order->billing_first_name;
+  $email = $order->billing_email;
+  $command = escapeshellcmd('python new_user.py freedomvpn '. $fname . ' '.$email);
+  
+}
