5de8ff324b30c3a2a3a2ad108c7e28a7635d1016
xUmaRix <umarizulkifli@gmail.com>
2016-05-17 18:11:03 +0800

    Fix path

--- wc_pritunl.php
+++ wc_pritunl.php
@@ -15,13 +15,13 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 //referal from : http://stackoverflow.com/questions/28218580/woocommerce-hook-for-after-payment-complete-actions
 
 
-add_filter( 'woocommerce_order_status_completed', 'create_user', 1);
-function create_user( $order_id ){
+add_filter( 'woocommerce_order_status_completed', 'new_user', 1);
+function new_user( $order_id ){
   global $woocommerce;
   
   $order = new WC_Order( $order_id );
   $fname = $order->billing_first_name;
   $email = $order->billing_email;
-  $command = escapeshellcmd('python /home/ubuntu/pritunl-script/new_user.py freedomvpn '. $fname . ' '.$email);
+  $command = escapeshellcmd('python /home/ubuntu/pritunl-script/new_user.py '. $fname .' '.$email);
   
 }
