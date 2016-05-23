86950e704182564340400e084483ca7db0adbd39
xUmaRix <umarizulkifli@gmail.com>
2016-05-23 13:20:19 +0800

    Update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # wc-pritunl
+
+Plugin to create user,get key,enable and disable user for Pritunl VPN via Wordpress
--- wc_pritunl.php
+++ wc_pritunl.php
@@ -22,6 +22,49 @@ function new_user( $order_id ){
   $order = new WC_Order( $order_id );
   $fname = $order->billing_first_name;
   $email = $order->billing_email;
-  $command = escapeshellcmd('python /home/ubuntu/pritunl-script/new_user.py '. $fname .' '.$email);
-  shell_exec($command);	  
+  $command = escapeshellcmd('python /home/ubuntu/pritunl-script/new_user.py '.$fname.' '.$email);
+  shell_exec($command);  
 }
+
+add_filter( 'woocommerce_email_after_order_table', 'add_link_back_to_order', 1);
+function add_link_back_to_order( $order_id) {
+ 	
+	$order = new WC_Order( $order_id);
+	$email = $order->billing_email;
+ 	$command2 = escapeshellcmd('python /home/ubuntu/pritunl-script/get_key.py '.$email);
+  	exec($command2,$key);
+	// Open the section with a paragraph so it is separated from the other content
+	$link = '<p>';
+ 
+	// Add the anchor link with the admin path to the order page
+	$link .= '<a href="'.$key.'">';
+ 
+	// Clickable text
+	$link .= __( 'Click here to download you VPN key', 'your_domain' );
+ 
+	// Close the link
+	$link .= '</a>';
+ 
+	// Close the paragraph
+	$link .= '</p>';
+ 
+	// Return the link into the email
+	echo $link;
+}
+
+add_filter( 'subscription_expired', 'disable_user', 1);
+function disable_user($order_id){
+	$order = new WC_Order( $order_id );
+	$email = $order->billing_email;
+	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/dis_usr.py '.$email);
+  	shell_exec($command);  
+}
+
+add_filter('woocommerce_renewal_order_payment_complete','enable_user',1);
+function enable_user($order_id){
+	$order = new WC_Order( $order_id );
+	$email = $order->billing_email;
+	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/en_usr.py '.$email);
+  	shell_exec($command);  
+}
+
