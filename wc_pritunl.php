31030ed3faf8e4bb85265dc38ad2972f8e1a66b6
xUmaRix <umarizulkifli@gmail.com>
2016-06-08 10:55:50 +0800

    Update to ver 2.0

--- wc_pritunl.php
+++ wc_pritunl.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: WC Pritunl
-Plugin URI: http://freedom.globaltransit.net/
+Plugin URI: http://www.hotantz.com/
 Description: Integration with Pritunl API
 Author: Muhammad Umari Zulkifli
-Author URI: http://freedom.globaltransit.net
-Version: 1.0
+Author URI: http://www.hotantz.com/
+Version: 2.0
 Text Domain: wootickets-asia-info
 
 Copyright: � 2016 xUmaRix
@@ -15,68 +15,55 @@ License URI: http://www.gnu.org/licenses/gpl-3.0.html
 //referal from : http://stackoverflow.com/questions/28218580/woocommerce-hook-for-after-payment-complete-actions
 
 
-add_filter('woocommerce_created_customer', 'new_user', 1);
-add_filter('woocommerce_order_status_completed_to_pending', 'disable_user', 1);
-add_filter('woocommerce_order_status_completed_to_on-hold', 'disable_user', 1);
-add_filter('subscriptions_activated_for_order','enable_user',1);
-add_filter('processed_subscription_payment_failure','disable_user',1);
-add_filter('subscriptions_expired_for_order','disable_user',1);
-add_filter('subscription_expired','disable_user',1);
-add_filter('subscription_put_on-hold','disable_user',1);
-add_filter('activated_subscription','enable_user',1);
-add_filter('failed_subscription_sign_ups_for_order','disable_user',1);
-add_filter('subscriptions_cancelled_for_order','disable_user',1);
-add_filter('subscription_trial_end','disable_user',1);
-add_filter('cancelled_subscription','disable_user',1);
-add_filter('subscription_end_of_prepaid_term','disable_user',1);
-add_filter('subscriptions_cancelled_for_order','disable_user',1);
-add_filter('woocommerce_subscriptions_deactivated','disable_user', 1);
-add_filter('woocommerce_delete_order_item','delete_user',1);
-add_filter('woocommerce_renewal_order_payment_complete','enable_user',1);
-add_filter('woocommerce_order_status_pending_to_completed', 'enable_user', 1);
-add_filter('woocommerce_order_status_on-hold_to_completed', 'enable_user', 1);
-add_filter('woocommerce_email_before_order_table', 'add_vpn_key_link_to_order', 1);
+add_action('user_register', 'new_user', 1);
+add_action('delete_user', 'delete_user', 1);
 
-function new_user( $order_id ){
-	global $woocommerce;
-	$order = new WC_Order( $order_id );
-	$fname = $order->billing_first_name;
-	$email = $order->billing_email;
+add_action('subscription_expired','disable_user',1);
+add_action('subscription_put_on-hold','disable_user',1);
+add_action('activated_subscription','enable_user',1);
+add_action('subscription_trial_end','disable_user',1);
+add_action('cancelled_subscription','disable_user',1);
+add_action('woocommerce_order_details_after_customer_details','add_key_link', 10, 1);
+
+function new_user( $user_id ){
+	$user_info = get_userdata($user_id);
+ 	$email = $user_info->user_email;
+	$fname = $user_info->user_login;
+	$get_key = escapeshellcmd('python /home/ubuntu/pritunl-script/get_key.py '.$email);
+	$key = shell_exec($get_key);
+	if ( ! $key ){
 	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/new_user.py '.$fname.' '.$email);
-	shell_exec($command);  
+	shell_exec($command);
+	}  
 }
 
-function add_vpn_key_link_to_order( $order_id, $sent_to_admin) {
-	global $woocommerce;
- 	if ( ! $sent_to_admin ) {
-	$order = new WC_Order( $order_id);
+function add_key_link( $order_id) {
+	$order = new WC_Order( $order_id );
 	$email = $order->billing_email;
  	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/get_key.py '.$email);
   	$key = shell_exec($command);
-	echo '<p>VPN Key Download Link: ' . $key . '</p>';	
+	if ( $key ){
+	echo '<table class="shop_table shop_table_responsive customer_details"><tbody><tr><th>VPN Key Download Link</th><td data-title="VPN Link"><a href ='.$key.'</a>Download</td></tr></tbody</table>';	
 	}
 }
 
-function disable_user($order_id){
-	global $woocommerce;
-	$order = new WC_Order( $order_id );
-	$email = $order->billing_email;
+function disable_user($user_id){
+	$user_info = get_userdata($user_id);
+ 	$email = $user_info->user_email;
 	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/dis_usr.py '.$email);
   	shell_exec($command);  
 }
 
-function enable_user($order_id){
-	global $woocommerce;
-	$order = new WC_Order( $order_id );
-	$email = $order->billing_email;
+function enable_user($user_id){
+	$user_info = get_userdata($user_id);
+ 	$email = $user_info->user_email;
 	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/en_usr.py '.$email);
   	shell_exec($command);  
 }
 
-function delete_user($order_id){
-	global $woocommerce;
-	$order = new WC_Order( $order_id );
-	$email = $order->billing_email;
+function delete_user($user_id){
+	$user_info = get_userdata($user_id);
+ 	$email = $user_info->user_email;
 	$command = escapeshellcmd('python /home/ubuntu/pritunl-script/del_usr.py '.$email);
   	shell_exec($command);  
 }
